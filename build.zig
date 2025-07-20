742e0ae59f8b005533b84fee31d7cf130f872236
Ivan Stepanov <ivanstepanovftw@gmail.com>
2025-07-20 15:08:12 +0200

    Update to Zig 0.15.0-dev.1147+69cf40da6

--- build.zig
+++ build.zig
@@ -11,19 +11,25 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = entry,
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = entry,
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const demo = b.addTest(.{
         .name = "demo",
-        .root_source_file = b.path("demos.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("demos.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "datetime", .module = lib },
+            },
+        }),
     });
-    demo.root_module.addImport("datetime", lib);
     const run_demo = b.addRunArtifact(demo);
 
     const test_step = b.step("test", "Run unit tests");
